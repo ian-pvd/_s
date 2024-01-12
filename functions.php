fb34b9a47da5a4ca18d00ef3d99c129068ae90a9
Ian Del Giudice <ian.pvd@gmail.com>
2024-01-12 18:46:34 -0500

    move js to source

--- functions.php
+++ functions.php
@@ -141,7 +141,7 @@ function _s_scripts() {
 	wp_enqueue_style( '_s-style', get_template_directory_uri() . '/build/style.css', array(), _S_VERSION );
 	wp_style_add_data( '_s-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/src/js/navigation.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
